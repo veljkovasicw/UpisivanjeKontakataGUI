f793ca8c8b09935f3466928eb81ca3cd19f20819
Marko <mare.radovanovic90@gmail.com>
2020-04-10 23:49:16 +0200

    Dodati Regexi Za SacuvajBtn Event u Sceni2

--- Vezbanje GUI/src/model/SacuvajBtnEvent.java
+++ Vezbanje GUI/src/model/SacuvajBtnEvent.java
@@ -1,6 +1,9 @@
 package model;
 
+import java.math.MathContext;
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -34,8 +37,9 @@ public class SacuvajBtnEvent implements EventHandler<javafx.event.ActionEvent> {
 	private Scene prvaScene;
 
 	public SacuvajBtnEvent(TextField imeTextField, TextField prezimeTextField, TextField adresaTextField,
-			TextField emailTextField, TextField brojeviTextField, ListaBrojeva listaBrojeva, GrupeKontaktaCombo comboScena2,
-			ListaImenaIPrezimena listaImenaIPrezimena , ComboBox<String> comboScena1 , Stage primaryStage, Scene prvaScene) {
+			TextField emailTextField, TextField brojeviTextField, ListaBrojeva listaBrojeva,
+			GrupeKontaktaCombo comboScena2, ListaImenaIPrezimena listaImenaIPrezimena, ComboBox<String> comboScena1,
+			Stage primaryStage, Scene prvaScene) {
 
 		this.imeTextField = imeTextField;
 		this.prezimeTextField = prezimeTextField;
@@ -45,9 +49,9 @@ public class SacuvajBtnEvent implements EventHandler<javafx.event.ActionEvent> {
 		this.listaBrojeva = listaBrojeva;
 		this.comboScena2 = comboScena2;
 		this.listaImenaIPrezimena = listaImenaIPrezimena;
-		this.comboScena1=comboScena1;
-		this.primaryStage=primaryStage;
-		this.prvaScene=prvaScene;
+		this.comboScena1 = comboScena1;
+		this.primaryStage = primaryStage;
+		this.prvaScene = prvaScene;
 
 	}
 
@@ -59,75 +63,112 @@ public class SacuvajBtnEvent implements EventHandler<javafx.event.ActionEvent> {
 		String email = emailTextField.getText();
 		String broj = brojeviTextField.getText();
 
-		ArrayList<Brojevi> brojevi = new ArrayList<Brojevi>();
+		// ovde definisem regexse za svaki String
+		String regexImeIliPrezime = "([A-Z][a-z]*)";
+		String regexAdresa = "([A-Z][a-z]*)\\s([A-Za-z]*)?\\s?\\d+";
+		String regexEmail = "(?:(?:\\r\\n)?[ \\t])*(?:(?:(?:[^()<>@,;:\\\\\".\\[\\] \\000-\\031]+(?:(?:(?:\\r\\n)?[ \\t])+|\\Z|(?=[\\[\"()<>@,;:\\\\\".\\[\\]]))|\"(?:[^\\\"\\r\\\\]|\\\\.|(?:(?:\\r\\n)?[ \\t]))*\"(?:(?:\\r\\n)?[ \\t])*)(?:\\.(?:(?:\\r\\n)?[ \\t])*(?:[^()<>@,;:\\\\\".\\[\\] \\000-\\031]+(?:(?:(?:\\r\\n)?[ \\t])+|\\Z|(?=[\\[\"()<>@,;:\\\\\".\\[\\]]))|\"(?:[^\\\"\\r\\\\]|\\\\.|(?:(?:\\r\\n)?[ \\t]))*\"(?:(?:\\r\\n)?[ \\t])*))*@(?:(?:\\r\\n)?[ \\t])*(?:[^()<>@,;:\\\\\".\\[\\] \\000-\\031]+(?:(?:(?:\\r\\n)?[ \\t])+|\\Z|(?=[\\[\"()<>@,;:\\\\\".\\[\\]]))|\\[([^\\[\\]\\r\\\\]|\\\\.)*\\](?:(?:\\r\\n)?[ \\t])*)(?:\\.(?:(?:\\r\\n)?[ \\t])*(?:[^()<>@,;:\\\\\".\\[\\] \\000-\\031]+(?:(?:(?:\\r\\n)?[ \\t])+|\\Z|(?=[\\[\"()<>@,;:\\\\\".\\[\\]]))|\\[([^\\[\\]\\r\\\\]|\\\\.)*\\](?:(?:\\r\\n)?[ \\t])*))*|(?:[^()<>@,;:\\\\\".\\[\\] \\000-\\031]+(?:(?:(?:\\r\\n)?[ \\t])+|\\Z|(?=[\\[\"()<>@,;:\\\\\".\\[\\]]))|\"(?:[^\\\"\\r\\\\]|\\\\.|(?:(?:\\r\\n)?[ \\t]))*\"(?:(?:\\r\\n)?[ \\t])*)*\\<(?:(?:\\r\\n)?[ \\t])*(?:@(?:[^()<>@,;:\\\\\".\\[\\] \\000-\\031]+(?:(?:(?:\\r\\n)?[ \\t])+|\\Z|(?=[\\[\"()<>@,;:\\\\\".\\[\\]]))|\\[([^\\[\\]\\r\\\\]|\\\\.)*\\](?:(?:\\r\\n)?[ \\t])*)(?:\\.(?:(?:\\r\\n)?[ \\t])*(?:[^()<>@,;:\\\\\".\\[\\] \\000-\\031]+(?:(?:(?:\\r\\n)?[ \\t])+|\\Z|(?=[\\[\"()<>@,;:\\\\\".\\[\\]]))|\\[([^\\[\\]\\r\\\\]|\\\\.)*\\](?:(?:\\r\\n)?[ \\t])*))*(?:,@(?:(?:\\r\\n)?[ \\t])*(?:[^()<>@,;:\\\\\".\\[\\] \\000-\\031]+(?:(?:(?:\\r\\n)?[ \\t])+|\\Z|(?=[\\[\"()<>@,;:\\\\\".\\[\\]]))|\\[([^\\[\\]\\r\\\\]|\\\\.)*\\](?:(?:\\r\\n)?[ \\t])*)(?:\\.(?:(?:\\r\\n)?[ \\t])*(?:[^()<>@,;:\\\\\".\\[\\] \\000-\\031]+(?:(?:(?:\\r\\n)?[ \\t])+|\\Z|(?=[\\[\"()<>@,;:\\\\\".\\[\\]]))|\\[([^\\[\\]\\r\\\\]|\\\\.)*\\](?:(?:\\r\\n)?[ \\t])*))*)*:(?:(?:\\r\\n)?[ \\t])*)?(?:[^()<>@,;:\\\\\".\\[\\] \\000-\\031]+(?:(?:(?:\\r\\n)?[ \\t])+|\\Z|(?=[\\[\"()<>@,;:\\\\\".\\[\\]]))|\"(?:[^\\\"\\r\\\\]|\\\\.|(?:(?:\\r\\n)?[ \\t]))*\"(?:(?:\\r\\n)?[ \\t])*)(?:\\.(?:(?:\\r\\n)?[ \\t])*(?:[^()<>@,;:\\\\\".\\[\\] \\000-\\031]+(?:(?:(?:\\r\\n)?[ \\t])+|\\Z|(?=[\\[\"()<>@,;:\\\\\".\\[\\]]))|\"(?:[^\\\"\\r\\\\]|\\\\.|(?:(?:\\r\\n)?[ \\t]))*\"(?:(?:\\r\\n)?[ \\t])*))*@(?:(?:\\r\\n)?[ \\t])*(?:[^()<>@,;:\\\\\".\\[\\] \\000-\\031]+(?:(?:(?:\\r\\n)?[ \\t])+|\\Z|(?=[\\[\"()<>@,;:\\\\\".\\[\\]]))|\\[([^\\[\\]\\r\\\\]|\\\\.)*\\](?:(?:\\r\\n)?[ \\t])*)(?:\\.(?:(?:\\r\\n)?[ \\t])*(?:[^()<>@,;:\\\\\".\\[\\] \\000-\\031]+(?:(?:(?:\\r\\n)?[ \\t])+|\\Z|(?=[\\[\"()<>@,;:\\\\\".\\[\\]]))|\\[([^\\[\\]\\r\\\\]|\\\\.)*\\](?:(?:\\r\\n)?[ \\t])*))*\\>(?:(?:\\r\\n)?[ \\t])*)|(?:[^()<>@,;:\\\\\".\\[\\] \\000-\\031]+(?:(?:(?:\\r\\n)?[ \\t])+|\\Z|(?=[\\[\"()<>@,;:\\\\\".\\[\\]]))|\"(?:[^\\\"\\r\\\\]|\\\\.|(?:(?:\\r\\n)?[ \\t]))*\"(?:(?:\\r\\n)?[ \\t])*)*:(?:(?:\\r\\n)?[ \\t])*(?:(?:(?:[^()<>@,;:\\\\\".\\[\\] \\000-\\031]+(?:(?:(?:\\r\\n)?[ \\t])+|\\Z|(?=[\\[\"()<>@,;:\\\\\".\\[\\]]))|\"(?:[^\\\"\\r\\\\]|\\\\.|(?:(?:\\r\\n)?[ \\t]))*\"(?:(?:\\r\\n)?[ \\t])*)(?:\\.(?:(?:\\r\\n)?[ \\t])*(?:[^()<>@,;:\\\\\".\\[\\] \\000-\\031]+(?:(?:(?:\\r\\n)?[ \\t])+|\\Z|(?=[\\[\"()<>@,;:\\\\\".\\[\\]]))|\"(?:[^\\\"\\r\\\\]|\\\\.|(?:(?:\\r\\n)?[ \\t]))*\"(?:(?:\\r\\n)?[ \\t])*))*@(?:(?:\\r\\n)?[ \\t])*(?:[^()<>@,;:\\\\\".\\[\\] \\000-\\031]+(?:(?:(?:\\r\\n)?[ \\t])+|\\Z|(?=[\\[\"()<>@,;:\\\\\".\\[\\]]))|\\[([^\\[\\]\\r\\\\]|\\\\.)*\\](?:(?:\\r\\n)?[ \\t])*)(?:\\.(?:(?:\\r\\n)?[ \\t])*(?:[^()<>@,;:\\\\\".\\[\\] \\000-\\031]+(?:(?:(?:\\r\\n)?[ \\t])+|\\Z|(?=[\\[\"()<>@,;:\\\\\".\\[\\]]))|\\[([^\\[\\]\\r\\\\]|\\\\.)*\\](?:(?:\\r\\n)?[ \\t])*))*|(?:[^()<>@,;:\\\\\".\\[\\] \\000-\\031]+(?:(?:(?:\\r\\n)?[ \\t])+|\\Z|(?=[\\[\"()<>@,;:\\\\\".\\[\\]]))|\"(?:[^\\\"\\r\\\\]|\\\\.|(?:(?:\\r\\n)?[ \\t]))*\"(?:(?:\\r\\n)?[ \\t])*)*\\<(?:(?:\\r\\n)?[ \\t])*(?:@(?:[^()<>@,;:\\\\\".\\[\\] \\000-\\031]+(?:(?:(?:\\r\\n)?[ \\t])+|\\Z|(?=[\\[\"()<>@,;:\\\\\".\\[\\]]))|\\[([^\\[\\]\\r\\\\]|\\\\.)*\\](?:(?:\\r\\n)?[ \\t])*)(?:\\.(?:(?:\\r\\n)?[ \\t])*(?:[^()<>@,;:\\\\\".\\[\\] \\000-\\031]+(?:(?:(?:\\r\\n)?[ \\t])+|\\Z|(?=[\\[\"()<>@,;:\\\\\".\\[\\]]))|\\[([^\\[\\]\\r\\\\]|\\\\.)*\\](?:(?:\\r\\n)?[ \\t])*))*(?:,@(?:(?:\\r\\n)?[ \\t])*(?:[^()<>@,;:\\\\\".\\[\\] \\000-\\031]+(?:(?:(?:\\r\\n)?[ \\t])+|\\Z|(?=[\\[\"()<>@,;:\\\\\".\\[\\]]))|\\[([^\\[\\]\\r\\\\]|\\\\.)*\\](?:(?:\\r\\n)?[ \\t])*)(?:\\.(?:(?:\\r\\n)?[ \\t])*(?:[^()<>@,;:\\\\\".\\[\\] \\000-\\031]+(?:(?:(?:\\r\\n)?[ \\t])+|\\Z|(?=[\\[\"()<>@,;:\\\\\".\\[\\]]))|\\[([^\\[\\]\\r\\\\]|\\\\.)*\\](?:(?:\\r\\n)?[ \\t])*))*)*:(?:(?:\\r\\n)?[ \\t])*)?(?:[^()<>@,;:\\\\\".\\[\\] \\000-\\031]+(?:(?:(?:\\r\\n)?[ \\t])+|\\Z|(?=[\\[\"()<>@,;:\\\\\".\\[\\]]))|\"(?:[^\\\"\\r\\\\]|\\\\.|(?:(?:\\r\\n)?[ \\t]))*\"(?:(?:\\r\\n)?[ \\t])*)(?:\\.(?:(?:\\r\\n)?[ \\t])*(?:[^()<>@,;:\\\\\".\\[\\] \\000-\\031]+(?:(?:(?:\\r\\n)?[ \\t])+|\\Z|(?=[\\[\"()<>@,;:\\\\\".\\[\\]]))|\"(?:[^\\\"\\r\\\\]|\\\\.|(?:(?:\\r\\n)?[ \\t]))*\"(?:(?:\\r\\n)?[ \\t])*))*@(?:(?:\\r\\n)?[ \\t])*(?:[^()<>@,;:\\\\\".\\[\\] \\000-\\031]+(?:(?:(?:\\r\\n)?[ \\t])+|\\Z|(?=[\\[\"()<>@,;:\\\\\".\\[\\]]))|\\[([^\\[\\]\\r\\\\]|\\\\.)*\\](?:(?:\\r\\n)?[ \\t])*)(?:\\.(?:(?:\\r\\n)?[ \\t])*(?:[^()<>@,;:\\\\\".\\[\\] \\000-\\031]+(?:(?:(?:\\r\\n)?[ \\t])+|\\Z|(?=[\\[\"()<>@,;:\\\\\".\\[\\]]))|\\[([^\\[\\]\\r\\\\]|\\\\.)*\\](?:(?:\\r\\n)?[ \\t])*))*\\>(?:(?:\\r\\n)?[ \\t])*)(?:,\\s*(?:(?:[^()<>@,;:\\\\\".\\[\\] \\000-\\031]+(?:(?:(?:\\r\\n)?[ \\t])+|\\Z|(?=[\\[\"()<>@,;:\\\\\".\\[\\]]))|\"(?:[^\\\"\\r\\\\]|\\\\.|(?:(?:\\r\\n)?[ \\t]))*\"(?:(?:\\r\\n)?[ \\t])*)(?:\\.(?:(?:\\r\\n)?[ \\t])*(?:[^()<>@,;:\\\\\".\\[\\] \\000-\\031]+(?:(?:(?:\\r\\n)?[ \\t])+|\\Z|(?=[\\[\"()<>@,;:\\\\\".\\[\\]]))|\"(?:[^\\\"\\r\\\\]|\\\\.|(?:(?:\\r\\n)?[ \\t]))*\"(?:(?:\\r\\n)?[ \\t])*))*@(?:(?:\\r\\n)?[ \\t])*(?:[^()<>@,;:\\\\\".\\[\\] \\000-\\031]+(?:(?:(?:\\r\\n)?[ \\t])+|\\Z|(?=[\\[\"()<>@,;:\\\\\".\\[\\]]))|\\[([^\\[\\]\\r\\\\]|\\\\.)*\\](?:(?:\\r\\n)?[ \\t])*)(?:\\.(?:(?:\\r\\n)?[ \\t])*(?:[^()<>@,;:\\\\\".\\[\\] \\000-\\031]+(?:(?:(?:\\r\\n)?[ \\t])+|\\Z|(?=[\\[\"()<>@,;:\\\\\".\\[\\]]))|\\[([^\\[\\]\\r\\\\]|\\\\.)*\\](?:(?:\\r\\n)?[ \\t])*))*|(?:[^()<>@,;:\\\\\".\\[\\] \\000-\\031]+(?:(?:(?:\\r\\n)?[ \\t])+|\\Z|(?=[\\[\"()<>@,;:\\\\\".\\[\\]]))|\"(?:[^\\\"\\r\\\\]|\\\\.|(?:(?:\\r\\n)?[ \\t]))*\"(?:(?:\\r\\n)?[ \\t])*)*\\<(?:(?:\\r\\n)?[ \\t])*(?:@(?:[^()<>@,;:\\\\\".\\[\\] \\000-\\031]+(?:(?:(?:\\r\\n)?[ \\t])+|\\Z|(?=[\\[\"()<>@,;:\\\\\".\\[\\]]))|\\[([^\\[\\]\\r\\\\]|\\\\.)*\\](?:(?:\\r\\n)?[ \\t])*)(?:\\.(?:(?:\\r\\n)?[ \\t])*(?:[^()<>@,;:\\\\\".\\[\\] \\000-\\031]+(?:(?:(?:\\r\\n)?[ \\t])+|\\Z|(?=[\\[\"()<>@,;:\\\\\".\\[\\]]))|\\[([^\\[\\]\\r\\\\]|\\\\.)*\\](?:(?:\\r\\n)?[ \\t])*))*(?:,@(?:(?:\\r\\n)?[ \\t])*(?:[^()<>@,;:\\\\\".\\[\\] \\000-\\031]+(?:(?:(?:\\r\\n)?[ \\t])+|\\Z|(?=[\\[\"()<>@,;:\\\\\".\\[\\]]))|\\[([^\\[\\]\\r\\\\]|\\\\.)*\\](?:(?:\\r\\n)?[ \\t])*)(?:\\.(?:(?:\\r\\n)?[ \\t])*(?:[^()<>@,;:\\\\\".\\[\\] \\000-\\031]+(?:(?:(?:\\r\\n)?[ \\t])+|\\Z|(?=[\\[\"()<>@,;:\\\\\".\\[\\]]))|\\[([^\\[\\]\\r\\\\]|\\\\.)*\\](?:(?:\\r\\n)?[ \\t])*))*)*:(?:(?:\\r\\n)?[ \\t])*)?(?:[^()<>@,;:\\\\\".\\[\\] \\000-\\031]+(?:(?:(?:\\r\\n)?[ \\t])+|\\Z|(?=[\\[\"()<>@,;:\\\\\".\\[\\]]))|\"(?:[^\\\"\\r\\\\]|\\\\.|(?:(?:\\r\\n)?[ \\t]))*\"(?:(?:\\r\\n)?[ \\t])*)(?:\\.(?:(?:\\r\\n)?[ \\t])*(?:[^()<>@,;:\\\\\".\\[\\] \\000-\\031]+(?:(?:(?:\\r\\n)?[ \\t])+|\\Z|(?=[\\[\"()<>@,;:\\\\\".\\[\\]]))|\"(?:[^\\\"\\r\\\\]|\\\\.|(?:(?:\\r\\n)?[ \\t]))*\"(?:(?:\\r\\n)?[ \\t])*))*@(?:(?:\\r\\n)?[ \\t])*(?:[^()<>@,;:\\\\\".\\[\\] \\000-\\031]+(?:(?:(?:\\r\\n)?[ \\t])+|\\Z|(?=[\\[\"()<>@,;:\\\\\".\\[\\]]))|\\[([^\\[\\]\\r\\\\]|\\\\.)*\\](?:(?:\\r\\n)?[ \\t])*)(?:\\.(?:(?:\\r\\n)?[ \\t])*(?:[^()<>@,;:\\\\\".\\[\\] \\000-\\031]+(?:(?:(?:\\r\\n)?[ \\t])+|\\Z|(?=[\\[\"()<>@,;:\\\\\".\\[\\]]))|\\[([^\\[\\]\\r\\\\]|\\\\.)*\\](?:(?:\\r\\n)?[ \\t])*))*\\>(?:(?:\\r\\n)?[ \\t])*))*)?;\\s*)";
+		String regexBroj = "^\\+(?:[0-9] ?){9,13}[0-9]$";
 
-		ArrayList<String> lista = listaBrojeva.getTableViewValues(listaBrojeva);
+		Pattern patternImeIliPrezime = Pattern.compile(regexImeIliPrezime);
+		Pattern patternAdresa = Pattern.compile(regexAdresa);
+		Pattern patternEmail = Pattern.compile(regexEmail);
+		Pattern patternBroj = Pattern.compile(regexBroj);
 
-		if (!lista.isEmpty()) {
+		Matcher matcherIme = patternImeIliPrezime.matcher(ime);
+		Matcher matcherPrezime = patternImeIliPrezime.matcher(prezime);
+		Matcher matcherAdresa = patternAdresa.matcher(adresa);
+		Matcher matcherEmail = patternEmail.matcher(email);
+		Matcher matcherBroj = patternBroj.matcher(broj);
 
-			for (String brojIzTabele : lista) {
+		// onda radim proveru sa if string != matachuje sa regexom break!?
+		if (matcherIme.matches()) {
+			System.out.println("Ime je ok!");
+		}
+		if (matcherPrezime.matches()) {
+			System.out.println("Prezime je ok!");
+		}
+		if (matcherAdresa.matches()) {
+			System.out.println("Adresa je ok!");
+		}
+		if (matcherEmail.matches()) {
+			System.out.println("Email je ok!");
+		}
+		if (matcherBroj.matches()) {
+			System.out.println("Broj je ok!");
+		}
+		if (matcherIme.matches() && matcherPrezime.matches() && matcherAdresa.matches() && matcherEmail.matches()
+				&& (matcherBroj.matches() || brojeviTextField.getText().isEmpty())) {
+			ArrayList<Brojevi> brojevi = new ArrayList<Brojevi>();
 
-				brojevi.add(new Brojevi(brojIzTabele));
+			ArrayList<String> lista = listaBrojeva.getTableViewValues(listaBrojeva);
+
+			if (!lista.isEmpty()) {
+
+				for (String brojIzTabele : lista) {
+
+					brojevi.add(new Brojevi(brojIzTabele));
+
+				}
 
 			}
 
-		}
+			if (broj != null) {
 
-		if (broj != null) {
-
-			brojevi.add(new Brojevi(broj));
-
-		}
-
-		Osoba osoba = new Osoba(ime, prezime, adresa, email, brojevi);
-
-		String kojiTrazim = comboScena2.getValue().toString();
-
-		ObservableList<String> grupeLista = FXCollections.observableArrayList();
-
-		for (Kontakti kontakt : comboScena2.getKontakti()) {
-
-			grupeLista.add(kontakt.toString() + " - " + (kontakt.getKontakti().size()));
-
-			if (kontakt.toString().equals(kojiTrazim)) {
-
-				kontakt.getKontakti().add(osoba);
-
-				System.out.println(kontakt.toString());
-				System.out.println("Uspesno nasao kontakt");
-
-				System.out.println(osoba);
-
-				System.out.println(kontakt.getKontakti().size());
+				brojevi.add(new Brojevi(broj));
 
 			}
 
+			Osoba osoba = new Osoba(ime, prezime, adresa, email, brojevi);
+
+			String kojiTrazim = comboScena2.getValue().toString();
+
+			ObservableList<String> grupeLista = FXCollections.observableArrayList();
+
+			for (Kontakti kontakt : comboScena2.getKontakti()) {
+
+				grupeLista.add(kontakt.toString() + " - " + (kontakt.getKontakti().size()));
+
+				if (kontakt.toString().equals(kojiTrazim)) {
+
+					kontakt.getKontakti().add(osoba);
+
+					System.out.println(kontakt.toString());
+					System.out.println("Uspesno nasao kontakt");
+
+					System.out.println(osoba);
+
+					System.out.println(kontakt.getKontakti().size());
+
+				}
+
+			}
+
+			System.out.println("YEEESSS");
+
+			imeTextField.clear();
+			prezimeTextField.clear();
+			adresaTextField.clear();
+			emailTextField.clear();
+			brojeviTextField.clear();
+			listaBrojeva.getItems().clear();
+			listaImenaIPrezimena.getItems().clear();
+			/*
+			 * 
+			 * scena1.getCombo().setItems(grupeLista);
+			 * scena1.getCombo().setValue(grupeLista.get(0));
+			 */
+
+			/* return grupeLista; */
+
+			comboScena1.setItems(grupeLista);
+
+			comboScena1.setValue(grupeLista.get(0));
+
+			primaryStage.setScene(prvaScene);
+			primaryStage.show();
+
 		}
-
-		System.out.println("YEEESSS");
-
-		imeTextField.clear();
-		prezimeTextField.clear();
-		adresaTextField.clear();
-		emailTextField.clear();
-		brojeviTextField.clear();
-		listaBrojeva.getItems().clear();
-		listaImenaIPrezimena.getItems().clear();
-		/*
-		 * 
-		 * scena1.getCombo().setItems(grupeLista);
-		 * scena1.getCombo().setValue(grupeLista.get(0));
-		 */
-
-		/* return grupeLista; */
-
-		comboScena1.setItems(grupeLista);
-
-		comboScena1.setValue(grupeLista.get(0));
-
-		primaryStage.setScene(prvaScene);
-		primaryStage.show();
-
+		
 	}
 
 }
